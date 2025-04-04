130c88339ad27141fd00f03c50bfe67fdf0da634
Kaitlyn Johnson <kej38@georgetown.edu>
2025-04-04 15:01:30 +0100

    more fixes

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,16 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
+
 library(testthat)
 library(evalwwforecasts)
 
-test_results <- test_check("evalwwforecasts")
+test_check("evalwwforecasts")
 
 if (any(as.data.frame(test_results)$warning > 0)) {
-  stop("tests failed with warnings")
+  stop("tests failed with warnings", call. = FALSE)
 }
