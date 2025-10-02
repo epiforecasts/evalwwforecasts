c39e16ba5ff070a3e37ab458950816feb01b6206
ciaramccarthy1 <ciara.mccarthy1@lshtm.ac.uk>
2025-10-02 12:10:26 +0100

    score quantiles and samples

--- targets/scoring_targets.R
+++ targets/scoring_targets.R
@@ -17,7 +17,7 @@ scoring_targets <- list(
 
   # Target to generate quantiles and log-transform
   tar_target(
-    name = hosp_for_scoring,
+    name = hosp_quantiles_for_scoring,
     command = draws_for_scoring(
       draws = hosp_draws_w_data,
       forecast_date = scenarios$forecast_date,
@@ -27,13 +27,35 @@ scoring_targets <- list(
     pattern = map(hosp_draws_w_data, scenarios)
   ),
 
-  # Target for scoring
+  # Target to generate samples and log-transform
   tar_target(
-    name = score_hosp,
+    name = hosp_samples_for_scoring,
+    command = draws_for_scoring(
+      draws = hosp_draws_w_data,
+      forecast_date = scenarios$forecast_date,
+      offset = 1,
+      quantiles = FALSE
+    ),
+    pattern = map(hosp_draws_w_data, scenarios)
+  ),
+
+  # Target for scoring quantiles
+  tar_target(
+    name = score_hosp_quantiles,
     command = generate_scores(
-      draws = hosp_for_scoring,
+      draws = hosp_quantiles_for_scoring,
       metrics = sample_metrics_quantiles
     ),
-    pattern = map(hosp_for_scoring, scenarios)
+    pattern = map(hosp_quantiles_for_scoring, scenarios)
+  ),
+
+  # Target for scoring
+  tar_target(
+    name = score_hosp_samples,
+    command = generate_scores(
+      draws = hosp_samples_for_scoring,
+      metrics = sample_metrics_quantiles
+    ),
+    pattern = map(hosp_samples_for_scoring, scenarios)
   )
 )
