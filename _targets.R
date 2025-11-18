000c44afd7041a8e6ccb7d7cfc38e13ae6f9e18d
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-11-18 10:02:41 +0000

    set up auto detect of cores based on 4 paraell chains

--- _targets.R
+++ _targets.R
@@ -28,7 +28,8 @@ functions <- list.files(here("R"), full.names = TRUE)
 walk(functions, source)
 rm("functions")
 
-plan(multisession, workers = 2)
+n_workers <- as.integer(floor(future::availableCores() / 4))
+plan(multisession, workers = n_workers)
 
 # load target modules
 targets <- list.files(here("targets"), full.names = TRUE)
